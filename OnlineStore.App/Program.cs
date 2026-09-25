ff24e9c2ec1254fa2e516072676d4fbb27fb47f3
Anastasia Kukhar <kukhar.nastya@gmail.com>
2026-09-25 18:56:20 +0300

    Add console application

--- OnlineStore.App/Program.cs
+++ OnlineStore.App/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Modern Programming Course");
+Console.WriteLine();
+Console.WriteLine("Student: Кухар Анастасія Романівна ");
+Console.WriteLine("Group: 401ТК");
+Console.WriteLine();
+Console.WriteLine("Variant: 13(1)");
+Console.WriteLine("Domain: Online Store");
+Console.WriteLine();
+Console.WriteLine(".NET: 10");
